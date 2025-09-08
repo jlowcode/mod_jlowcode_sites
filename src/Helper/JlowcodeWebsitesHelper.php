cae012fe9389328e91a1bfe3b679e2b792a74d63
Diego-Godina <diegosantos.2001@alunos.utfpr.edu.br>
2025-09-08 10:42:31 -0300

    feta: added a lot of things

--- src/Helper/JlowcodeWebsitesHelper.php
+++ src/Helper/JlowcodeWebsitesHelper.php
@@ -19,6 +19,7 @@ use Joomla\Database\DatabaseAwareTrait;
 use Joomla\Registry\Registry;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
+use Gantry\Framework\Gantry;
 
 /**
  * Helper for mod_jlowcode_websites
@@ -28,8 +29,8 @@ class JlowcodeWebsitesHelper implements DatabaseAwareInterface
 {
     use DatabaseAwareTrait;
 
-    private $websiteTable = 'sites';
-    private $tableRepeatable;
+    private $websiteTable;
+    private $menuItensTable;
 
     /**
      * Returns the layout data.
@@ -45,8 +46,9 @@ class JlowcodeWebsitesHelper implements DatabaseAwareInterface
             return [];
         }
 
-        $this->setTableRepeatable();
-        if(empty($this->tableRepeatable)) {
+        $this->setTableMenuItensList();
+        $this->setTableWebsiteList();
+        if(empty($this->websiteTable) || empty($this->menuItensTable)) {
             return [];
         }
 
@@ -68,7 +70,7 @@ class JlowcodeWebsitesHelper implements DatabaseAwareInterface
     }
 
     /**
-     * This method render the website menu using the mod-menu module
+     * This method render the website menu
      * 
      * @param       object                      &$websiteData       Website data to add the menu
      * @param       CMSApplicationInterface     $app                Application
@@ -77,16 +79,33 @@ class JlowcodeWebsitesHelper implements DatabaseAwareInterface
      */
     private function renderMenu(&$websiteData, $app)
     {
-        $module          = new \stdClass();
-        $module->id      = 0;
-        $module->module  = 'mod_menu';
-        $module->params  = new Registry([
-            'menutype' => $this->getMenuType($websiteData->id_parent_menutype),
-            'layout'   => 'default',
-            'startLevel' => 2
-        ]);
+        $gantry = Gantry::instance();
+        $theme = $gantry['theme'];
+        $menu = $gantry['menu'];
 
-        $websiteData->menu = ModuleHelper::renderModule($module);
+        $menuName = $this->getMenuType($websiteData->id_parent_menutype);
+        $menuConfig = [
+            'menu' => $menuName,
+            'startLevel' => 2,
+            'enabled' => 1,
+            'maxLevels' => 0,
+            'renderTitles' => 0,
+            'base' => $websiteData->id_separator_menu_item
+        ];
+        $items = $this->getMenuItems($websiteData->url);
+        $config = $menu->config();
+        $config->set('settings.type', 'custom');
+        $menu->addCustom(array(), $items);
+
+        $config->merge(array('items' => $items));
+        $config->merge(array('ordering' => $items));
+        $config->get('items');
+        $instanceMenu = $menu->instance(array(), $config);
+
+        $websiteData->menu =  $theme->render('@particles/menu.html.twig', [
+            'menu' => $instanceMenu,
+            'particle' => $menuConfig
+        ]);
     }
 
     /**
@@ -141,17 +160,52 @@ class JlowcodeWebsitesHelper implements DatabaseAwareInterface
     {
         $db = $this->getDatabase();
 
-        $input = $app->input;
-        $listId = $input->getInt('listid');
+        $listId = $this->getListId($app);
 
         $query = $db->getQuery(true);
-        $query->select($db->qn('parent_id'))
-            ->from($db->qn($this->tableRepeatable))
+        $query->select($db->qn('site'))
+            ->from($db->qn($this->menuItensTable))
             ->where($db->qn('menu_list') . ' = ' . $db->q($listId));
         $db->setQuery($query);
         $websiteId = $db->loadResult();
 
         return (int) $websiteId;
+    }
+
+    private function getMenuItems($menuType)
+    {
+        $modelItem = Factory::getApplication()->bootComponent('com_menus')->getMVCFactory()->createModel('Item', 'Administrator');
+        $app = Factory::getApplication();
+
+        $modelItem->getState(); 	//We need do this to set __state_set before the save
+	    $menu = $app->getMenu();
+        $url = 'index.php?option=com_fabrik&view=list&listid=' . $this->getListId($app);
+        $attributes = ['menutype'];
+        $values = [$menuType];
+        $menuItems = $menu->getItems($attributes, $values, false);
+
+        $items = array();
+        $removeItensWithThisAlias = array();
+
+        // Get the alias of itens with -form or -details to remove the menu itens that have the same alias without this sufix
+        foreach ($menuItems as $item) {
+            if(stripos($item->alias, '-form') !== false || stripos($item->alias, '-details') !== false) {
+                $removeItensWithThisAlias[] = str_ireplace('-form', '', str_ireplace('-details', '', $item->alias));
+            }
+
+            $dataMenuItems[] = (array) $modelItem->getItem($item->id);
+        }
+
+        // This is to avoid duplicate itens in menu
+        foreach ($removeItensWithThisAlias as $alias) {
+            foreach ($dataMenuItems as $item) {
+                if($item['alias'] != $alias) {
+                    $items[] = $item;
+                }
+            }
+        }
+
+        return $items;
     }
 
     /**
@@ -164,13 +218,11 @@ class JlowcodeWebsitesHelper implements DatabaseAwareInterface
     private function checkIsSubPage(CMSApplicationInterface $app)
     {
         $db = $this->getDatabase();
-
-        $input = $app->input;
-        $listId = $input->getInt('listid');
+        $listId = $this->getListId($app);
 
         $query = $db->getQuery(true);
         $query->select('COUNT(*)')
-            ->from($db->qn($this->tableRepeatable))
+            ->from($db->qn($this->menuItensTable))
             ->where($db->qn('menu_list') . ' = ' . $db->q($listId));
         $db->setQuery($query);
         $isSubPage = $db->loadResult();
@@ -179,34 +231,91 @@ class JlowcodeWebsitesHelper implements DatabaseAwareInterface
     }
 
     /**
-     * This method search for $this->table in #__fabrik_lists and set the repeatable table for use in querys
+     * This method set the table of menu itens list for use in query
      * 
      * @return      null
      */
-    private function setTableRepeatable()
+    private function setTableMenuItensList()
     {
-		$joinModel = Factory::getApplication()->bootComponent('com_fabrik')->getMVCFactory()->createModel('Join', 'FabrikFEModel');
-		$listModel = Factory::getApplication()->bootComponent('com_fabrik')->getMVCFactory()->createModel('List', 'FabrikFEModel');
+        $listModelMenuItens = Factory::getApplication()->bootComponent('com_fabrik')->getMVCFactory()->createModel('List', 'FabrikFEModel');
+        $listModelMenuItens->setId($this->getIdListMenuItens());
 
-        $db = $this->getDatabase();
+        $formModelMenuItens = $listModelMenuItens->getFormModel();
+        $this->menuItensTable = $formModelMenuItens->getTableName();
+    }
+
+    /**
+     * This method set the table of website list for use in query
+     * 
+     * @return      null
+     */
+    private function setTableWebsiteList()
+    {
+        $listModelWebsite = Factory::getApplication()->bootComponent('com_fabrik')->getMVCFactory()->createModel('List', 'FabrikFEModel');
+        $listModelWebsite->setId($this->getIdListWebsite());
+
+        $formModelWebsite = $listModelWebsite->getFormModel();
+        $this->websiteTable = $formModelWebsite->getTableName();
+    }
+
+    /**
+     * This method get the id of the website list
+     * 
+     * @return      int
+     */
+    private function getIdListWebsite()
+    {
+        $db = Factory::getContainer()->get('DatabaseDriver');
 
         $query = $db->getQuery(true);
         $query->select($db->qn('id'))
             ->from($db->qn('#__fabrik_lists'))
-            ->where($db->qn('db_table_name') . ' = ' . $db->q($this->websiteTable));
+            ->where($db->qn('db_table_name') . ' = ' . $db->q('sites'));
         $db->setQuery($query);
         $websitesListId = $db->loadResult();
 
-        if(empty($websitesListId)) {
-            return;
+        return $websitesListId;
+    }
+
+    /**
+     * This method get the id of the menu itens list
+     * 
+     * @return      int
+     */
+    private function getIdListMenuItens()
+    {
+        $db = Factory::getContainer()->get('DatabaseDriver');
+
+        $query = $db->getQuery(true);
+        $query->select($db->qn('id'))
+            ->from($db->qn('#__fabrik_lists'))
+            ->where($db->qn('db_table_name') . ' = ' . $db->q('itens_do_menu'));
+        $db->setQuery($query);
+        $menuItensListId = $db->loadResult();
+
+        return $menuItensListId;
+    }
+
+    /**
+     * This method get the list id from input or from form id
+     * 
+     * @param       CMSApplicationInterface     $app        Application
+     * 
+     * @return      int
+     */
+    private function getListId($app)
+    {
+        $formModel = Factory::getApplication()->bootComponent('com_fabrik')->getMVCFactory()->createModel('Form', 'FabrikFEModel');
+
+        $input = $app->input;
+        $listId = $input->getInt('listid');
+
+        if(!$listId) {
+            $formId = $input->getInt('formid');
+            $formModel->setId($formId);
+            $listId = $formModel->getListModel()->getId();
         }
 
-		$listModel->setId($websitesListId);
-        $formModel = $listModel->getFormModel();
-        $groups = $formModel->getPublishedGroups();
-        $idGroup = array_values($groups)[1]->id;           // Repeat group must be the second group created
-        $joinModel->setId($groups[$idGroup]->join_id);
-
-        $this->tableRepeatable = $joinModel->getJoin()->table_join;
+        return $listId;
     }
 }
